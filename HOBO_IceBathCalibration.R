c710a006fa02117aa269d44aeb3ecff6e283ada0
Kathryn A Gannon <kag326@cornell.edu>
2023-02-17 13:20:00 -0500

    add set up

--- HOBO_IceBathCalibration.R
+++ HOBO_IceBathCalibration.R
@@ -4,4 +4,20 @@
 # Holgerson Lab; Katie Gannon February 2023
 
 # 0. Set Up R Environment
- 
+# ______________________________________________________________________________________________________________________________________________________
+# 0. Set up R Environment 
+library(dplyr)
+library(sf)
+library(raster)
+library(ape) 
+suppressMessages(library(mosaic))
+suppressMessages(library(readxl))
+library(tidyverse)
+library(devtools)
+library(lme4)
+library(car)
+library(effects)
+library(mosaic)
+library(writexl)
+library(purrr)
+library(ggplot2)
