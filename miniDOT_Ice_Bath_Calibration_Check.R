728fdb5f60906df6bcd80e67423dc7748581411f
Kathryn A Gannon <kag326@cornell.edu>
2023-02-17 13:19:11 -0500

    adding ice bath calibration check code

---
+++ miniDOT_Ice_Bath_Calibration_Check.R
@@ -0,0 +1,187 @@
+######################################
+# Script for visualizing the results of miniDOT calibration check 
+######################################
+# Holgerson Lab - Gannon 02-10-2023
+
+# ______________________________________________________________________________________________________________________________________________________
+# 0. Set up R Environment 
+library(dplyr)
+library(sf)
+library(raster)
+library(ape) 
+suppressMessages(library(mosaic))
+suppressMessages(library(readxl))
+library(tidyverse)
+library(devtools)
+library(lme4)
+library(car)
+library(effects)
+library(mosaic)
+library(writexl)
+library(purrr)
+library(ggplot2)
+#install.packages("ggpmisc") <- try on desktop 
+# library(ggpmisc)
+
+setwd("~/Equipment/miniDOT_Calibration_Check")
+
+# 1.  Read in miniDOT data  
+setwd("~/Equipment/miniDOT_Calibration_Check/BubbleBathData_021023") #Reset the working directory to the folder containing just the files that need to be formatted
+file_names <- list.files(pattern="*.TXT") #Get a list of all of the .txt files in the working directory 
+list_of_miniDOT_SN <- lapply(file_names, read.table, skip = , header = T, sep = ",", fill = TRUE)
+list_of_miniDOT_data <- lapply(file_names, read.table, skip = 6, header = T, sep = ",", fill = TRUE) #this pulls in the data but looses the serial numbers   
+setwd("~/Equipment/miniDOT_Calibration_Check")    # Reset the working directory to the base 
+
+# 2.  Formatt each dataframe of miniDOT data 
+
+# Write a function to clean up the raw miniDOT data and get it in a format that is easier to work with in R
+miniDOT_cleaning_FUNC <- function(df){
+  names(df) <- c("Unix.Timestamp_sec", "UTC_Date_Time", "EST", "Batter_volt", "Temp_C", "Dissolved_Oxygen_mg_l", "Dissolved_Oxygen_Sat_%", "Q")
+  df2 <- df[-1, ]
+  head(df2)
+  df2 <- subset(df2, select = c("EST", "Temp_C", "Dissolved_Oxygen_mg_l", "Dissolved_Oxygen_Sat_%"))
+  names(df2) <- c("Date_Time","Temp_C", "DO_mgl", "DOSat_per")
+  head(df2)
+  df2$Temp_C <- as.numeric(df2$Temp_C)
+  df2$DO_mgl <- as.numeric(df2$DO_mgl)
+  df2$DOSat_per <- as.numeric(df2$DOSat_per)
+  df2$Date_Time <- as.character(df2$Date_Time)  # Needs to be as a character to pass through forloop bellow
+  #df2$Date_Time  <- as.POSIXct(df2$Date_Time, tz = "EST", format = "%Y-%m-%d %H:%M")
+  formatted_output <- as.data.frame(df2)
+}
+
+# Test that you r function works 
+test_df <- list_of_miniDOT_data[[8]]
+test_output <- miniDOT_cleaning_FUNC(test_df)
+head(test_output)
+
+# Apply your cleaning function over the full list of miniDOT data 
+list_cleaned_miniDOT_data <- lapply(list_of_miniDOT_data, miniDOT_cleaning_FUNC)
+head(list_cleaned_miniDOT_data[[6]]) #Check that cleaning everything together worked 
+
+
+# 3. Extract meta data so that you can connect to serial number 
+
+# Extract MetaData  and add them to each file 
+head(list_of_miniDOT_SN[[1]])
+df_a <- list_of_miniDOT_SN[[1]]
+head(df_a)
+names(df_a)
+
+# Write a function to extract the metadata for each miniDOT data file 
+extractMetaData_FUNC <- function(df_a){
+  sn_full <- df_a[1, "MiniDOT.Logger.Concatenated.Data.File"]
+  sn <- substring(sn_full, 21, 31)
+  concatDate_full <- df_a[2, "MiniDOT.Logger.Concatenated.Data.File"]
+  concatDate <- substring(concatDate_full, 21,43) 
+  output <- c(sn, concatDate)
+}
+
+# Test your metadata extraction function on one df 
+test_data <- list_of_miniDOT_SN[[4]]
+test_output <- extractMetaData_FUNC(test_data)
+test_output
+
+# Apply that metadata extraction function across all of the miniDOT SNs 
+metaData <- lapply(list_of_miniDOT_SN, extractMetaData_FUNC)
+
+# Format the output 
+metaData_df <- as.data.frame(metaData)
+metaData_df <- as.data.frame(t(metaData_df))
+names(metaData_df)
+names(metaData_df) <- c("SN", "Concat_Date")
+head(metaData_df)
+row_nums <- seq(1:nrow(metaData_df))
+row.names(metaData_df) <- row_nums
+row.names(metaData_df)
+
+# Name all of the dfs in your list of cleaned miniDOT data with their serial number 
+serial_numbers <- metaData_df$SN 
+names(list_cleaned_miniDOT_data) <- serial_numbers
+names(list_cleaned_miniDOT_data)
+
+serial_numbers
+
+# 4. Add serial number as a column in each data frame and put them all into one big data frame
+
+#Practice 
+str(list_of_miniDOT_data[[5]])
+
+clean_df1 <- list_cleaned_miniDOT_data[[1]]
+sn_df1 <- metaData_df[1, "SN"]
+clean_df1$Serial_Number <- sn_df1
+head(clean_df1)
+
+# Make a blank data frame to put the results into
+miniDOT_cleaned_data_sn <- data.frame(matrix(ncol = 5, nrow = 1))
+colnames(miniDOT_cleaned_data_sn) <- c("Date_Time", "Temp_C", "DO_mgl", "DOSat_per", "Serial_Number")
+
+# Run through a for loop to add serial number to each dataframe and add the data frames together into one big df 
+total_rows <- length(list_cleaned_miniDOT_data)
+row_seq <- seq(1:total_rows)
+
+for (i in row_seq ){
+  clean_df1 <- list_cleaned_miniDOT_data[[i]]
+  sn_df1 <- metaData_df[i, "SN"]
+  clean_df1$Serial_Number <- sn_df1
+  miniDOT_cleaned_data_sn <- rbind(miniDOT_cleaned_data_sn, clean_df1)
+}
+
+miniDOT_cleaned_data_sn <- miniDOT_cleaned_data_sn[-1,] #Delete the first row of NAs from when you made the blank data set  
+head(miniDOT_cleaned_data_sn) # Check 
+miniDOT_data <- as.data.frame(miniDOT_cleaned_data_sn)
+
+# Format the output 
+str(miniDOT_data)
+miniDOT_data$Serial_Number <- as.factor(miniDOT_data$Serial_Number)
+head(miniDOT_data)
+tally(~Serial_Number, data = miniDOT_data)
+miniDOT_data$Date_Time  <- as.POSIXct(miniDOT_data$Date_Time, tz = "EST", format = "%Y-%m-%d %H:%M")
+head(miniDOT_data)
+str(miniDOT_data)
+
+
+# 5. Trim to only the time window that you are interested in 
+
+# Round 1 
+# round1_SNs <- c("7392-941101", "7392-094016", "7392-149054", "7392-591332", "7450-913951", "7450-917361", "7450-960183", "7450-981145", "7392-936968","7450-919756", "7450-000560", "7450-654299", "7450-542721", "7450-543142")
+miniDOT_data_r1 <- miniDOT_data[miniDOT_data$Serial_Number %in% round1_SNs , ]
+start_time_r1 <- as.POSIXct("2023-02-09 15:00:00", tz = "EST", format = "%Y-%m-%d %H:%M")
+end_time_r1 <- as.POSIXct("2023-02-10 08:45:00", tz = "EST", format = "%Y-%m-%d %H:%M")
+miniDOT_data_trimmed_r1 <- miniDOT_data_r1[miniDOT_data_r1$Date_Time >= start_time_r1 & miniDOT_data_r1$Date_Time <= end_time_r1 , ]
+
+# Round 2 
+miniDOT_data_r2 <- miniDOT_data[!miniDOT_data$Serial_Number %in% round1_SNs , ]
+start_time_r2 <- as.POSIXct("2023-02-10 9:00:00", tz = "EST", format = "%Y-%m-%d %H:%M")
+end_time_r2 <- as.POSIXct("2023-02-10 15:00:00", tz = "EST", format = "%Y-%m-%d %H:%M")
+miniDOT_data_trimmed_r2 <- miniDOT_data_r2[miniDOT_data_r2$Date_Time >= start_time_r2 & miniDOT_data_r2$Date_Time <= end_time_r2 , ]
+
+
+
+# 6. Plot DO over time for the desired interval 
+
+# Round 1 of Calibration (first bucket in first time window)
+miniDOT_calibration_check_plot1  <- miniDOT_data_trimmed_r1 %>%
+  ggplot(aes(x=Date_Time, y = DOSat_per)) +
+  geom_line(aes(y = DOSat_per, color = Serial_Number)) + 
+  geom_point(aes(y = DOSat_per, color = Serial_Number)) + 
+  theme_bw() +
+  ylab("Dissolved Oxygen Saturation (%)") + xlab("Time") + ggtitle("miniDOT Calibration Check: Holgerson Lab and Alex")
+miniDOT_calibration_check_plot1 
+
+# Round 2 of Calibration (second bucket in first time window)
+miniDOT_calibration_check_plot2  <- miniDOT_data_trimmed_r2 %>%
+  ggplot(aes(x=Date_Time, y = DOSat_per)) +
+  geom_line(aes(y = DOSat_per, color = Serial_Number)) + 
+  geom_point(aes(y = DOSat_per, color = Serial_Number)) + 
+  theme_bw() +
+  ylab("Dissolved Oxygen Saturation (%)") + xlab("Time") + ggtitle("miniDOT Calibration Check: Sheel's Sensors")
+miniDOT_calibration_check_plot2 
+
+
+# 7. Save Output 
+?ggsave(0)
+setwd("~/Equipment/miniDOT_Calibration_Check/Output_Figures")
+
+ggsave("Calibration_Check_Plot_R1_Holgerson_and_Alex.png", miniDOT_calibration_check_plot1, width = 190, height = 120, units = "mm")
+ggsave("Calibration_Check_Plot_R2_Sheel.png", miniDOT_calibration_check_plot2, width = 190, height = 120, units = "mm")
