09e97dce953c6b834d7703ec4cbd386caac74375
Katie Ganon <54601749+KathrynAlta@users.noreply.github.com>
2023-05-09 18:02:01 -0400

    file for concatenating

---
+++ 03_miniDOT_Concatenate.R
@@ -0,0 +1,7 @@
+######################################
+# Concatenating MiniDOT Data  
+######################################
+# Holgerson Lab - Gannon 05-09-2023
+
+# Folder of folders for each of the loggers
+
